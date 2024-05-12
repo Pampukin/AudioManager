c4e4531724eb6f2373c4f9e0ffe923d0d6aa0bf4
Pampukin <20010903yuusuke@gmail.com>
2024-05-12 23:01:24 +0900

    オーディオタイプの追加

---
+++ Assets/AudioManager/AudioType/SE_Types.cs
@@ -0,0 +1,8 @@
+﻿// AbstractSoundClips.csで生成
+
+/// <summary> AudioType </summary>
+public enum SE_Types
+{
+	jump1,
+	lose1,
+}
---
+++ Assets/Scripts/AudioMixerManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer _audioMixer;
+    
+    [SerializeField]
+    private AudioSource _BGM_AudioSource, _SE_AudioSource;
+    
+}
